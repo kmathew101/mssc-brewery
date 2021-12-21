5db3c83619c855c124da0ad77d6a92852606c7f0
kmathew101 <kurianmathewp@hotmail.com>
2021-12-20 19:16:48 -0500

    Update 1

--- src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,6 +1,9 @@
 package guru.springframework.msscbrewery.services;
 
+
+import com.github.javafaker.Faker;
 import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -10,11 +13,15 @@ import java.util.UUID;
  */
 @Service
 public class BeerServiceImpl implements BeerService {
+
+    @Autowired
+    Faker faker;
+
     @Override
     public BeerDto getBeerById(UUID beerId) {
         return BeerDto.builder().id(UUID.randomUUID())
-                .beerName("Galaxy Cat")
-                .beerStyle("Pale Ale")
+                .beerName( faker.beer().name())
+                .beerStyle( faker.beer().style())
                 .build();
     }
 }
--- src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -1,6 +1,8 @@
 package guru.springframework.msscbrewery.services;
 
+import com.github.javafaker.Faker;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -10,11 +12,15 @@ import java.util.UUID;
  */
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    Faker faker;
+
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
         return CustomerDto.builder()
                 .id(UUID.randomUUID())
-                .name("Joe Buck")
+                .name(faker.name().lastName() + " " +  faker.name().firstName())
                 .build();
     }
 }
